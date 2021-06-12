17ebfabb74c7e8268ab901383864b3263e6236e1
Munavwar <munavwarbaig@yahoo.com>
2021-06-11 21:53:19 -0700

    added css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,16 +53,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
   update: function () {
     var tempArr = [];
@@ -119,7 +121,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -254,6 +256,14 @@ var auditTask = function (taskEl) {
   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  // checks on the date every 30 minutes to see if all date info is current
+  setInterval(function () {
+    $(".card .list-group-item").each(function (index, el) {
+      auditTask(el);
+    });
+  }, 1800000);
 };
+
 // load tasks for the first time
 loadTasks();
